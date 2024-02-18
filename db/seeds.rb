ab30c835a234a97476903e92d434c4f21cba5bb7
Morten Jacobsen <mortjac@mortens-air.lan>
2024-02-18 15:21:22 +0100

    Created a Category model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Category.create(name: "Sports")
+Category.create(name: "Music")
+Category.create(name: "Movies")
+Category.create(name: "Books")
+
